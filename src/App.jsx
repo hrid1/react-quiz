9bc0bad607e0c28bc0ed5c8a6982319f4ede75fc
hrid1 <hridoycse1@gmail.com>
2024-05-06 03:01:21 +0600

    routing done with class 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import Layout from "./components/Layout"
-import Home from "./components/pages/Home"
-import Login from "./components/pages/Login"
-import Quiz from "./components/pages/Quiz"
-import Signup from "./components/pages/Signup"
-import "./styles/App.css"
+import Layout from "./components/Layout";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Quiz from "./components/pages/Quiz";
+import Result from "./components/pages/Result";
+import Signup from "./components/pages/Signup";
+import "./styles/App.css";
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
 
 
 
 function App() {
-  
-
   return (
-    <Layout>
-      {/* <Home></Home> */}
-      {/* <Signup></Signup> */}
-      {/* <Login></Login> */}
-      <Quiz></Quiz>
-    </Layout>
-  )
+    <BrowserRouter>
+     <Layout>
+     <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/signup' element={<Signup/>}/>
+        <Route exact path='/login' element={<Login/>}/>
+        <Route exact path='/quiz' element={<Quiz/>}/>
+        <Route exact path='/result' element={<Result/>}/>
+      </Routes>
+     </Layout>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
